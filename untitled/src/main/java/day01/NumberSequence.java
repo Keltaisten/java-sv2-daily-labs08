81370b52c8985d399bd06f2f2c0070de675ab70d
Keltaisten <keltaisten@gmail.com>
2021-11-29 15:51:36 +0100

    constructor kinda ok

--- untitled/src/main/java/day01/NumberSequence.java
+++ untitled/src/main/java/day01/NumberSequence.java
@@ -3,13 +3,23 @@ package day01;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.Random;
 
 public class NumberSequence {
     private List<Integer> numbers;
 
     public NumberSequence(List<Integer> numbers) {
         this.numbers = Arrays.asList(1,2,3,4,5);
-        this.numbers = Arrays.asList(6,7,8,9,10);
+//        this.numbers = Arrays.asList(6,7,8,9,10);
+    }
+
+    public NumberSequence(int i1, int i2, int i3) {
+        List<Integer> randomNumberList = new ArrayList<>();
+        Random rand = new Random();
+        for(int i = 0; i < i1;i++){
+            randomNumberList.add(rand.nextInt(i2,i3));
+        }
+        this.numbers = randomNumberList;
     }
 
     public List<Integer> closeToAverage(int value){
